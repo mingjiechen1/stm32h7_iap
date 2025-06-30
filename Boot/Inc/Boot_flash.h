a1e5cf4edc68487c53b235922c963d034367f7bb
mj.chen <madan02@163.com>
2025-06-30 17:08:29 +0800

    添加了flash操作，还没验证。

--- Boot/Inc/Boot_flash.h
+++ Boot/Inc/Boot_flash.h
@@ -4,5 +4,27 @@
 #include    "Boot_include.h"
 
 
+ #define BOOT_PAGE_SIZE                         (0x20000U)    /* 128 Kbyte */
+ #define BOOT_FLASH_SIZE                        (0x100000U)   /* 1MB */
+
+uint16_t FLASH_ReadHalfWord(uint32_t Flash_Add);
+uint32_t FLASH_ReadWord(uint32_t Flash_Add);
+void STMFLASH_Write_NoCheck(uint32_t WriteAddr,uint16_t *pBuffer,uint16_t NumToWrite)   ;
+void FLASH_Read(uint32_t ReadAddr, uint16_t *pBuffer, uint16_t NumToRead);
+
+
+// HAL_StatusTypeDef HAL_FLASH_Unlock(void);
+// HAL_StatusTypeDef HAL_FLASH_Lock(void);
+// HAL_StatusTypeDef HAL_FLASH_OB_Unlock(void);
+// HAL_StatusTypeDef HAL_FLASH_OB_Lock(void);
+/* Program operation functions  ***********************************************/
+// HAL_StatusTypeDef HAL_FLASH_Program(uint32_t TypeProgram, uint32_t FlashAddress, uint32_t DataAddress);
+// HAL_StatusTypeDef HAL_FLASH_Program_IT(uint32_t TypeProgram, uint32_t FlashAddress, uint32_t DataAddress);
+// /* FLASH IRQ handler method */
+// void HAL_FLASH_IRQHandler(void);
+// /* Callbacks in non blocking modes */
+// void HAL_FLASH_EndOfOperationCallback(uint32_t ReturnValue);
+// void HAL_FLASH_OperationErrorCallback(uint32_t ReturnValue);
+
 
 #endif
--- Boot/Src/Boot_flash.c
+++ Boot/Src/Boot_flash.c
@@ -1,2 +1,30 @@
 #include    "Boot_flash.h"
 
+uint16_t FLASH_ReadHalfWord(uint32_t Flash_Add)
+{
+	return *(__IO uint16_t*)( Flash_Add );
+}
+
+uint32_t FLASH_ReadWord(uint32_t Flash_Add)
+{
+	return *(__IO uint32_t*)( Flash_Add );
+}
+
+void FLASH_Read(uint32_t ReadAddr, uint16_t *pBuffer, uint16_t NumToRead)
+{
+    uint16_t i;
+    for (i = 0; i < NumToRead; i++)
+    {
+        pBuffer[i] = FLASH_ReadHalfWord(ReadAddr + i * 2);
+    }
+}
+
+void STMFLASH_Write_NoCheck(uint32_t WriteAddr,uint16_t *pBuffer,uint16_t NumToWrite)   
+{ 			 		 
+	uint16_t i;
+	for(i=0;i<NumToWrite;i++)
+	{
+		HAL_FLASH_Program(FLASH_TYPEPROGRAM_FLASHWORD, WriteAddr, pBuffer[i]);
+	    WriteAddr+=2;//add addr 2.
+	}  
+} 
